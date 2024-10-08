8573d436a0c7ed41ff503e1306d606c9a53a2d6d
ZiaG22 <ziaove22@gmail.com>
2024-10-08 16:11:27 -0500

    add local timezone item and function

--- javascript/index.js
+++ javascript/index.js
@@ -23,6 +23,9 @@ newyorkTimeElement.innerHTML = moment()
 
   function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current"){
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElecment = document.querySelector("#cities");
